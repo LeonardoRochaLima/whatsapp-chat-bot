8e077776bde34364df46d9a8cdd300fdb13346f1
higorseixas <higor.c.seixas@gmail.com>
2023-03-10 13:18:14 -0300

    alterando servico

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { isValidNumber, parsePhoneNumber } from 'libphonenumber-js';
-import { Whatsapp, create } from 'venom-bot';
+import { Whatsapp, create, SocketState } from 'venom-bot';
 
+export type QRcode = {
+  base64Qrimg: string;
+  asciiQR: string;
+  attempts: number;
+};
 @Injectable()
 export class AppService {
   private client: Whatsapp;
+  private conected: boolean;
+  private qrCode: QRcode;
 
   constructor() {
     this.initialize();
+  }
+
+  get isConected(): boolean {
+    return this.conected;
+  }
+
+  get qrCodeQ(): QRcode {
+    return this.qrCode;
   }
 
   async sendText(to: string, body: string) {
     this.client.sendText(to, body).catch((error) => console.error(error));
   }
 
-  qr = (base64Qrimg: string) => {};
+  qr = (base64Qrimg: string, asciiQR: string, attempts: number) => {
+    this.qrCode = { base64Qrimg, asciiQR, attempts };
+  };
 
-  status = (statusSession: string, session: string) => {};
+  status = (statusSession: string, session: string) => {
+    this.conected = ['isLogged', 'qrReadSuccess', 'chatsAvailable'].includes(
+      statusSession
+    );
+  };
 
   validatePhoneNumber(number: string) {
     if (!isValidNumber(number, 'BR')) {
@@ -38,15 +59,14 @@ export class AppService {
     const numberToSend = this.validatePhoneNumber(process.env.NUMBER_TO_SEND);
     this.client = client;
     this.sendText(numberToSend, 'Olá, tudo bem?');
+    client.onStateChange((state) => {
+      this.conected = state === SocketState.CONNECTED;
+    });
   };
 
   private initialize() {
     create('whatsapp-chat-bot', this.qr, this.status)
       .then((client) => this.start(client))
       .catch((error) => console.error(error));
-  }
-
-  getHello(): string {
-    return 'Hello World!';
   }
 }
